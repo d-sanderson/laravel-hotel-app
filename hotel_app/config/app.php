ff87dd3a56bd6593bd23e8e93e1e4e72b8921ebf
dsanderson90 <dsanderson3@cnm.edu>
2019-01-12 13:07:00 -0700

    - practiced dependency injection from Title -> ClientController - created a dummy service provider and added to config/app.php

--- hotel_app/config/app.php
+++ hotel_app/config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+//TODO Insert Providers below !
         /*
          * Application Service Providers...
          */
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+		 App\Providers\DummyServiceProvider::class
 
     ],
 
--- hotel_app/routes/web.php
+++ hotel_app/routes/web.php
@@ -39,7 +39,27 @@ Route::get('/home', function () {
 
 Route::get('/di', 'ClientController@di');
 
+Route::get('/facades/db', function () {
 
+	return DB::select('SELECT * FROM TABLE');
+
+});
+
+Route::get('/facades/encrypt', function () {
+
+	return Crypt::encrypt('123456789');
+
+});
+
+//eyJpdiI6IjRtSFhSUEFJdTVJbzVXU1lrSUFnQVE9PSIsInZhbHVlIjoiWjZ3UTVBemJLZVdCWUwraW5BMjRtOFRJTWVac0lSMmVKb0NWNTNBclZRbz0iLCJtYWMiOiIyYTY1ZTdkNDI0MWY3ZGIzZmU2YTNmNTI3NzYwYThkYmMyYjNhNmFkYzgzNDkyODM4ZDIwODcyMTM2NTM4ZjY0In0
+
+Route::get('/facades/decrypt', function () {
+
+	return Crypt::decrypt('eyJpdiI6IjRtSFhSUEFJdTVJbzVXU1lrSUFnQVE9PSIsInZhbHVlIjoiWjZ3UTVBemJLZVdCWUwraW5BMjRtOFRJTWVac0lSMmVKb0NWNTNBclZRbz0iLCJtYWMiOiIyYTY1ZTdkNDI0MWY3ZGIzZmU2YTNmNTI3NzYwYThkYmMyYjNhNmFkYzgzNDkyODM4ZDIwODcyMTM2NTM4ZjY0In0');
+
+});
+
+//Facades simplify accessing certain classes however be careful not to overdo it.
 
 
 
